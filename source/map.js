3ddd31b4f90841df2fcbfc1076ed19659a1a56a2
Philippe Hugo <info@phlhg.ch>
2023-04-25 19:16:44 +0200

    Improve & Clean up: - Make permute a generator - Move optimization logic to bruteforce script

--- source/map.js
+++ source/map.js
@@ -7,18 +7,18 @@ export default class Map {
         this.dom.wrapper = wrapper;
         this.dom.svg = wrapper.querySelector("svg");
         this.dom.notice = wrapper.querySelector(".notice");
-
         this.dom.notice.classList.add("active");
 
         this.cities = []
-        this.best = 99999999999
-        this.bestseq = []
+        this.roads = []
+
+        this.clear()
     }
 
     clear() {
         this.cities = []
-        this.best = 99999999999
-        this.bestseq = []
+        this.roads = []
+        this.draw();
         this.dom.notice.classList.add("active");
     }
 
@@ -29,32 +29,27 @@ export default class Map {
 
     addCity(x, y){
         this.cities.push(new City(x, y, 500 + Math.round(Math.random() * Math.pow(10,6))))
-        this.best = 999999999999
-        this.bestseq = []
         this.dom.notice.classList.remove("active");
         return this.cities[this.cities.length - 1]
     }
 
+    addRoad(a,b){
+        this.roads.push([a,b]);
+        this.roads.push([b,a]);
+    }
+
     draw(){
         
-
         this.dom.svg.innerHTML = '' // Clear svg
 
         // Place roads
+        for(let i = 0; i < this.roads.length; i++){
 
+            let a = this.roads[i][0];
+            let b = this.roads[i][1];
 
-        /*
-        for(let i = 0; i < this.cities.length; i++){
-            let a = this.cities[i]
+            if(a.id > b.id){
 
-            let closest = Array.from(Array(this.cities.length).keys()).sort((i,j) => {
-                let d1 = Math.sqrt(Math.pow(a.x - this.cities[i].x, 2) + Math.pow(a.y - this.cities[i].y, 2))
-                let d2 = Math.sqrt(Math.pow(a.x - this.cities[j].x, 2) + Math.pow(a.y - this.cities[j].y, 2))
-                return d1 - d2
-            })
-
-            closest.slice(1, 4).forEach(j => {
-                let b = this.cities[j]
                 let l = document.createElementNS("http://www.w3.org/2000/svg", "line")
                 l.setAttribute('x1', a.x)
                 l.setAttribute('y1', a.y)
@@ -62,75 +57,29 @@ export default class Map {
                 l.setAttribute('y2', b.y)
                 l.setAttribute('class', 'road')
                 this.dom.svg.appendChild(l)
-            })
+
+            }
 
         }
-        */
 
         // Place cities
         for(let i = 0; i < this.cities.length; i++){
+
             let city = this.cities[i]
+
             let c = document.createElementNS("http://www.w3.org/2000/svg", "circle")
             c.setAttribute('cx', city.x)
             c.setAttribute('cy', city.y)
             c.setAttribute('r', 1 + 2*Math.log(city.size / 1000))
             c.setAttribute('class', 'city')
+
             let t = document.createElementNS("http://www.w3.org/2000/svg", "title")
             t.innerHTML = `Population: ${city.size.toLocaleString()}`
             c.appendChild(t)
+
             this.dom.svg.appendChild(c)
         }
 
-    }
-
-    drawEdges(c){
-        // simply connect the cities
-        var sum = 0;
-        for(var i = 0; i < c.length; i++){
-            var a = c[i];
-            var b = c[(i+1)%c.length];
-            var d = c[(i-1+c.length)%c.length];
-
-            [b, d].forEach(el => {
-                let d1 = Math.sqrt(Math.pow(a.x - el.x, 2) + Math.pow(a.y - el.y, 2))
-                sum += d1
-            });
-        }
-
-        if (sum < this.best) {
-            this.best = sum;
-            this.bestseq = c;
-
-            this.dom.svg.innerHTML = '' // Clear svg
-            for(let i = 0; i < this.cities.length; i++){
-                let city = this.cities[i]
-                let c = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-                c.setAttribute('cx', city.x)
-                c.setAttribute('cy', city.y)
-                c.setAttribute('r', 1 + 2*Math.log(city.size / 1000))
-                c.setAttribute('class', 'city')
-                let t = document.createElementNS("http://www.w3.org/2000/svg", "title")
-                t.innerHTML = `Population: ${city.size.toLocaleString()}`
-                c.appendChild(t)
-                this.dom.svg.appendChild(c)
-            }
-            
-            for(var i = 0; i < this.bestseq.length; i++){
-                var a = c[i];
-                var b = c[(i+1)%c.length];
-                var d = c[(i-1+c.length)%c.length];
-
-                [b, d].forEach(el => {
-                    let l = document.createElementNS("http://www.w3.org/2000/svg", "line")
-                    l.setAttribute('x1', a.x)
-                    l.setAttribute('y1', a.y)
-                    l.setAttribute('x2', el.x)
-                    l.setAttribute('y2', el.y)
-                    l.setAttribute('class', 'road')
-                    this.dom.svg.appendChild(l)
-                });
-            }
-        }
     }
 
 }
--- source/util/bruteforce.js
+++ source/util/bruteforce.js
@@ -1,17 +1,55 @@
 import {permute, sleep} from './helper'
-import Map from "./../map"
 
+var running = false;
 
-export function startbrute(map) {
-    let permutations = permute(map.cities)
-    console.log("got perms")
+function calcWeight(path){
 
-    for (let i = 0; i < permutations.length; i++) {
-        let c = permutations[i]
+    var weight = 0;
 
-        map.drawEdges(c)
-
-        //sleep(10)
+    for(let i = 0; i < path.length; i++){
+        let a = path[i];
+        let b = path[(i+1)%path.length]
+        weight += Math.sqrt(
+            Math.pow(a.x - b.x,2) + Math.pow(a.y - b.y,2)
+        );
     }
-    console.log("done")
+
+    return weight;
+
+}
+
+export async function startbrute(map) {
+
+    if(running){ return; }
+    running = true;
+
+    var opt = Infinity;
+    var opt_perm = [];
+
+    var perm_gen = permute([...map.cities])
+    var perm = perm_gen.next();
+
+    while(!perm.done){
+        var w = calcWeight(perm.value);
+
+        if(w < opt){
+            opt = w;
+            opt_perm = perm.value;
+
+            map.roads = [];
+            for(let i = 0; i < opt_perm.length; i++){
+                map.addRoad(opt_perm[i],opt_perm[(i+1)%opt_perm.length])
+            }
+            
+            map.draw();
+
+            await sleep(100);
+        }
+
+        perm = perm_gen.next();
+    }
+
+    console.log("Done")
+    running = false;
+
 }
