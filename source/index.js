246ff53e15ddc4047a0219e5d046675bc4ead7f5
Philippe Hugo <info@phlhg.ch>
2023-05-30 12:10:33 +0200

    Change test to a visualization of the routing

--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ import Bruteforce from "./methods/bruteforce/bruteforce";
 import Bruteforce2 from "./methods/bruteforce2/bruteforce2";
 import SimpleAnt from "./methods/simpleant/simpleant";
 import Genetic from "./methods/genetic/genetic";
-import Test from "./methods/test/test";
+import Routing from "./methods/routing/routing";
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -13,6 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     app.controls.addMethod(Bruteforce);
     app.controls.addMethod(SimpleAnt);
     app.controls.addMethod(Genetic)
-    app.controls.addMethod(Test,1);
+    app.controls.addMethod(Routing,1);
     app.controls.addMethod(Bruteforce2,1);
 })
---
+++ source/methods/routing/routing.js
@@ -0,0 +1,58 @@
+import { calculateTraffic, dist } from "../../map/utils";
+import Method from "../method"
+import { SliderSetting } from "../settings";
+
+export default class Routing extends Method {
+
+    constructor(...data){
+
+        super(...data);
+
+        this.name = "Routing";
+        this.description = "Connects close cities and calculates the traffic to visualize used the routing."
+
+        this.addSetting("sp_limit", SliderSetting, {
+            name: "Shortest Path Limit",
+            min: 2, max: 101, value: 10, step: 1,
+            formatter: v => { return v == 101 ? `None` : `${v}`}
+        })
+
+        this.addSetting("connectivity", SliderSetting, {
+            name: "Connectivity",
+            min: 2, max: 20, value: 4, step: 1,
+        })
+
+        this.done = false;
+
+    }
+
+    async run(){
+
+        this.done = true;
+
+        this.app.problem = "traffic";
+
+        this.app.graph.forEdges(e => { 
+            e.active = false; 
+            e.data.width = 10;
+            e.data.traffic = 0; 
+        }); // Reset all edges
+
+        this.app.graph.forNodes(n => {
+            let closest = this.app.graph.getNodes().sort((a,b) => { return dist(n,a) - dist(n,b); });
+            for(let v of closest.slice(1,this.getSetting("connectivity"))){ this.app.graph.getEdge(n.id,v.id).active = true; } 
+        })
+
+        let limit = this.getSetting("sp_limit") == 101 ? Infinity : this.getSetting("sp_limit");
+        var fitness = calculateTraffic(this.app.graph, limit);
+        console.log(fitness)
+
+        this.app.map.update();
+
+    }
+
+    async stop(){
+        this.done = true;
+    }
+
+}
